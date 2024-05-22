the bug in the inference computation is fixed, fixed-point convergence is guaranteed AND it ACTUALLY converges (woohoo), results are already performing better.

ImplicitEquiVSetFlax/main_flax.py
@@ -32,7 +32,7 @@ from torchvision import transforms
 # from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 
 from model.EquiVSet_trainer import EquiVSetTrainer
-from data_loader import TwoMoons, GaussianMixture,Amazon, CelebA, SetPDBBind, SetBindingDB
+from data_loader import TwoMoons, GaussianMixture, Amazon, CelebA, SetPDBBind, SetBindingDB
 import utils.config as config_file
 
 # jax.config.update("jax_debug_nans", True)  # stops execution when nan occurs
@@ -54,7 +54,9 @@ def get_data(params):
     elif data_name == 'bindingdb':
         return SetBindingDB(params)
     else:
-        raise ValueError("Invalid data_name. Supported options are: 'moons', 'gaussian', 'amazon', 'celeba', 'pdbbind', 'bindingdb'")
+        raise ValueError(
+            "Invalid data_name. Supported options are: 'moons', 'gaussian', 'amazon', 'celeba', 'pdbbind', 'bindingdb'")
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -73,7 +75,7 @@ def parse_arguments():
                         help='num layers [%(default)d]')
     parser.add_argument('--batch_size', type=int, default=128,
                         help='batch size [%(default)d]')
-    parser.add_argument('--lr', type=float, default=0.0001,
+    parser.add_argument('--lr', type=float, default=1e-4,
                         help='initial learning rate [%(default)g]')
     parser.add_argument("--weight_decay", type=float, default=1e-5,
                         help='weight decay rate [%(default)g]')
@@ -99,6 +101,8 @@ def parse_arguments():
                         help='num of RNN steps [%(default)d], K in the paper')
     parser.add_argument('--num_samples', type=int, default=5,
                         help='num of Monte Carlo samples [%(default)d]')
+    parser.add_argument('--derandomize', type=bool, default=False,
+                        help='Flag to determine degree of randomization of the Monte-Carlo sampling [%(default)d]')
     parser.add_argument('--rank', type=int, default=5,
                         help='rank of the perturbation matrix [%(default)d]')
     parser.add_argument('--tau', type=float, default=0.1,
@@ -113,12 +117,23 @@ def parse_arguments():
                         help='Implicit differentiation flag [%(default)d]')
     parser.add_argument('--bwd_solver', type=str, default='normal_cg',
                         choices=['normal_cg', 'gmres'],
-                        help='Backward solver choice to be used in backpropagation during implicit differentiation '
+                        help='Backward solver choice to be used in backpropagation during implicit differentiation aka '
+                             'solver of the linear system in implicit differentiation'
                              '[%(default)d]')
+    parser.add_argument('--bwd_maxiter', type=int, default=20,
+                        help='Number of fixed-point iterations [%(default)d]')
+    parser.add_argument('--bwd_tol', type=float, default=1e-2,
+                        help='Tolerance interval of fixed-point iterations [%(default)d]')
     parser.add_argument('--fwd_solver', type=str, default='fpi',
                         choices=['fpi', 'anderson'],
                         help='Forward solver choice to be used in forward pass during implicit differentiation '
                              '[%(default)d]')
+    parser.add_argument('--fwd_maxiter', type=int, default=1,
+                        help='Number of fixed-point iterations [%(default)d]')
+    parser.add_argument('--fwd_tol', type=float, default=1e-2,
+                        help='Tolerance interval of fixed-point iterations [%(default)d]')
+    parser.add_argument('--is_verbose', type=bool, default=False,
+                        help='Verbosity flag for JaxOPT fixed-point iterations [%(default)d]')
     args = parser.parse_args()
     return args
 
@@ -145,9 +160,11 @@ if __name__ == "__main__":
     batch_size = params.batch_size
     num_workers = params.num_workers
 
+
     def tensor_to_numpy(x):
         x = np.array(x, dtype=np.float32)
         return x
+
 
     train_loader, val_loader, test_loader = data.get_loaders(batch_size, num_workers, transform=tensor_to_numpy)
     # print(next(iter(train_loader)))
@@ -164,7 +181,7 @@ if __name__ == "__main__":
     metrics = trainer.train_model(train_loader,
                                   val_loader,
                                   test_loader=test_loader,
-                                  num_epochs=10)
+                                  num_epochs=params.epochs)
 
     print(f'Training Loss: {metrics["train/loss"]:.2f}')
     # print(f'Training Jaccard index: {metrics["train/jaccard"]}')

ImplicitEquiVSetFlax/model/EquiVSet_trainer.py
@@ -138,8 +138,8 @@ class EquiVSetTrainer(TrainerModule):
                 pre_mask = jnp.zeros([S_set.shape[-1]])
                 # call(lambda x: print(f"sum is {x}"), jnp.sum(S_set[i]))
                 s_size = jnp.sum(S_set[i]).astype(int)  # needs s_size as input
-                mask = jnp.where(jnp.arange(S_set.shape[-1]) < s_size, True, False)
-                ids = idx[i] * mask
+                mask = jnp.where(jnp.arange(S_set.shape[-1]) < s_size, True, self.model_hparams['params'].v_size + 1)
+                ids = (idx[i] + 1) * mask - 1
                 # call(lambda x: print(f"ids: {x}"), ids)
                 pre_mask = pre_mask.at[ids].set(1)  # , mode='drop')
                 # call(lambda x: print(f"pre_mask: {x}"), pre_mask)

ImplicitEquiVSetFlax/model/set_functions_flax.py
@@ -9,145 +9,10 @@ from jaxopt import AndersonAcceleration, FixedPointIteration
 from jaxopt.linear_solve import solve_gmres, solve_normal_cg
 from typing import Callable
 from utils.flax_helper import FF, normal_cdf
-from utils.implicit import SigmoidFixedPointLayer, SigmoidImplicitLayer
-
-
-# noinspection PyAttributeOutsideInit
-# class SetFunction(nn.Module):
-#     """
-#         Definition of the set function (F_θ) using a NN.
-#     """
-#     params: dict
-#     dim_feature: int = 256
-#
-#     def setup(self):
-#         self.init_layer = self.define_init_layer()
-#         self.ff = FF(self.dim_feature, 500, 1, self.params.num_layers)
-#         # self.fixed_point_layer = self.define_fixed_point_layer()
-#
-#     def define_init_layer(self):
-#         """
-#         Returns the initial layer custom to different setups.
-#         :return: InitLayer
-#         """
-#         return nn.Dense(features=self.dim_feature)
-#
-#     # def define_fixed_point_layer(self):
-#     #     """
-#     #
-#     #     :return:
-#     #     """
-#     #     if not self.params.IFT:
-#     #         return SigmoidFixedPointLayer(mfvi)
-#     #     if self.params.bwd_solver == 'normal_cg':
-#     #         implicit_solver = partial(solve_normal_cg, tol=1e-3, maxiter=20)
-#     #     if self.params.fwd_solver == 'fpi':
-#     #         fixed_point_solver = partial(FixedPointIteration,
-#     #                                      maxiter=5,
-#     #                                      tol=1e-3, implicit_diff=True,
-#     #                                      implicit_diff_solve=implicit_solver,
-#     #                                      verbose=True)
-#     #     return SigmoidImplicitLayer(mfvi=self.mean_field_iteration,
-#     #                                 fixed_point_solver=fixed_point_solver)
-#
-#     def MC_sampling(self, q, M, derandomize=False):
-#         """
-#         Bernoulli sampling using q as parameters.
-#         Args:
-#             q: parameter of Bernoulli distribution (ψ in the paper)
-#             M: number of samples (m in the paper)
-#
-#         Returns:
-#             Sampled subsets F(S+i), F(S)
-#             :param derandomize:
-#
-#         """
-#         bs, vs = q.shape
-#         if derandomize:
-#             # same sample is used for each coordinate
-#             q = jnp.broadcast_to(q.reshape(bs, 1, vs), (bs, M, vs))
-#         else:
-#             # a new sample is generated for each coordinate
-#             q = jnp.broadcast_to(q.reshape(bs, 1, 1, vs), (bs, M, vs, vs))
-#         # bs = the batch size
-#         q = jax.device_put(q)
-#         sample_matrix = jax.random.bernoulli(jax.random.PRNGKey(758493), q)
-#         if derandomize:
-#             sample_matrix = jnp.broadcast_to(sample_matrix.reshape(bs, M, 1, vs), (bs, M, vs, vs))
-#
-#         mask = jnp.expand_dims(jnp.concatenate([jnp.expand_dims(jnp.eye(vs, vs), axis=0) for _ in range(M)], axis=0),
-#                                axis=0)
-#
-#         matrix_0 = sample_matrix * (1 - mask)  # element_wise multiplication
-#         matrix_1 = matrix_0 + mask
-#         return matrix_1, matrix_0  # F([x]_+i), F([x]_- i)
-#
-#     def F_S(self, V, subset_mat, fpi=False):
-#         if fpi:
-#             fea = self.init_layer(V).reshape(subset_mat.shape[0], 1, -1, self.dim_feature)
-#         else:
-#             # to encode variational dist
-#             fea = self.init_layer(V).reshape(subset_mat.shape[0], -1, self.dim_feature)
-#         fea = subset_mat @ fea
-#         fea = self.ff(fea)  # goes through FF block
-#         return fea
-#
-#     def grad_F_S(self, V, subset_i, subset_not_i):
-#         F_1 = self.F_S(V, subset_i, fpi=True).squeeze(-1)
-#         F_0 = self.F_S(V, subset_not_i, fpi=True).squeeze(-1)
-#         return (F_1 - F_0).mean(1)
-#
-#     def mean_field_iteration(self, q, V):  # ψ_i in the paper
-#         subset_i, subset_not_i = self.MC_sampling(q, self.params.num_samples)  # returns S+i , S
-#         q = jax.nn.sigmoid(self.grad_F_S(V, subset_i, subset_not_i))
-#         return q
-#
-#     def cross_entropy(self, q, S, neg_S):  # Eq. (5) in the paper
-#         loss = - jnp.sum((S * jnp.log(q + 1e-12) + (1 - S) * jnp.log(1 - q + 1e-12)) * neg_S, axis=-1)
-#         return loss.mean()
-#
-#     def __call__(self, V, S, neg_S, **kwargs):
-#         """"returns cross-entropy loss."""
-#         bs, vs = V.shape[:2]
-#         q_init = .5 * jnp.ones((bs, vs))  # ψ_0 <-- 0.5 * vector(1)
-#         # if not self.params.IFT:
-#         #     q, _, _ = self.fixed_point_layer(q_init, V)
-#         # else:
-#         #     q = self.fixed_point_layer(q_init, V)
-#         if self.params.bwd_solver == 'normal_cg':
-#             implicit_solver = partial(solve_normal_cg, tol=1e-3, maxiter=20)
-#         if self.params.fwd_solver == 'fpi':
-#             fixed_point_solver = partial(FixedPointIteration,
-#                                          maxiter=5,
-#                                          tol=1e-3, implicit_diff=True,
-#                                          implicit_diff_solve=implicit_solver,
-#                                          verbose=True)
-#
-#         solver = fixed_point_solver(fixed_point_fun=self.mean_field_iteration)
-#         q = solver.run(q_init, V).params
-#
-#         # q = q_init
-#         # iterations = 0
-#         # last_err = float('inf')
-#         #
-#         # # iterate until convergence
-#         # while iterations < 20:
-#         #     q_next = self.mfvi(q, V)
-#         #     err = jnp.linalg.norm(q - q_next)
-#         #     print(err)
-#         #     q = q_next
-#         #     iterations += 1
-#         #     # if err < 1e-3:
-#         #     #     break
-#         #     last_err = err
-#
-#         loss = self.cross_entropy(q, S, neg_S)
-#         return loss
+from utils.implicit import SigmoidImplicitLayer
 
 
 class MFVI(nn.Module):
-    # init_layer: Callable  # nn.Module
-    # ff: Callable  # nn.Module
     params: dict
     dim_feature: int = 256
 
@@ -171,7 +36,6 @@ class MFVI(nn.Module):
         else:
             # a new sample is generated for each coordinate
             q = jnp.broadcast_to(q.reshape(bs, 1, 1, vs), (bs, M, vs, vs))
-        # bs = the batch size
         q = jax.device_put(q)
         sample_matrix = jax.random.bernoulli(jax.random.PRNGKey(758493), q)
         if derandomize:
@@ -182,7 +46,7 @@ class MFVI(nn.Module):
 
         matrix_0 = sample_matrix * (1 - mask)  # element_wise multiplication
         matrix_1 = matrix_0 + mask
-        return matrix_1, matrix_0  # F([x]_+i), F([x]_- i)
+        return matrix_1, matrix_0, sample_matrix  # F([x]_+i), F([x]_- i)
 
     def define_init_layer(self):
         """
@@ -191,27 +55,14 @@ class MFVI(nn.Module):
         """
         return nn.Dense(features=self.dim_feature)
 
-    # def F_S(self, V, subset_mat, fpi=False):
-    #     if fpi:
-    #         fea = self.init_layer(V).reshape(subset_mat.shape[0], 1, -1, self.dim_feature)
-    #     else:
-    #         # to encode variational dist
-    #         fea = self.init_layer(V).reshape(subset_mat.shape[0], -1, self.dim_feature)
-    #     fea = subset_mat @ fea
-    #     fea = self.ff(fea)  # goes through FF block
-    #     return fea
-    #
-    # def grad_F_S(self, V, subset_i, subset_not_i):
-    #     F_1 = self.F_S(V, subset_i, fpi=True).squeeze(-1)
-    #     F_0 = self.F_S(V, subset_not_i, fpi=True).squeeze(-1)
-    #     return (F_1 - F_0).mean(1)
-
     @nn.compact
     def __call__(self, q, V):  # ψ_i in the paper
-        subset_i, subset_not_i = self.MC_sampling(q, self.params.num_samples)  # returns S+i , S
+        # returns S+i , S
+        subset_i, subset_not_i, _ = self.MC_sampling(q, self.params.num_samples, derandomize=self.params.derandomize)
         init_layer = self.define_init_layer()
         ff = FF(self.dim_feature, 500, 1, self.params.num_layers)
-
+        # print(V.shape)
+        # print(init_layer(V).shape)  # this shape should be (1024, 256) instead of (1024, 64, 64, 256)
         fea_1 = init_layer(V).reshape(subset_i.shape[0], 1, -1, self.dim_feature)
         fea_1 = subset_i @ fea_1
         fea_1 = ff(fea_1).squeeze(-1)
@@ -220,9 +71,15 @@ class MFVI(nn.Module):
         fea_0 = subset_not_i @ fea_0
         fea_0 = ff(fea_0).squeeze(-1)
 
-        # q = jax.nn.sigmoid(self.grad_F_S(V, subset_i, subset_not_i))
-        q = jax.nn.sigmoid((fea_1 - fea_0).mean(1))
+        grad = (fea_1 - fea_0).mean(1)
+        l2_norm = jnp.linalg.norm(grad)
+        # if l2_norm > 2/self.params.v_size:
+        #     grad *= 2 / (self.params.v_size * l2_norm)
+        grad = jnp.where(l2_norm > 2/self.params.v_size, (2 / (self.params.v_size * l2_norm)) * grad, grad)
+
+        q = jax.nn.sigmoid(grad)
         return q
+
 
 class CrossEntropy(nn.Module):
     @nn.compact
@@ -240,64 +97,36 @@ class SetFunction(nn.Module):
     dim_feature: int = 256
 
     def setup(self):
-        # self.init_layer = self.define_init_layer()
-        # self.ff = FF(self.dim_feature, 500, 1, self.params.num_layers)
-        # self.mfvi = MFVI(self.init_layer, self.ff, self.params)
         self.mfvi = MFVI(self.params)
         self.fixed_point_layer = self.define_fixed_point_layer(self.mfvi)
         self.cross_entropy = CrossEntropy()
-
-    # def define_init_layer(self):
-    #     """
-    #     Returns the initial layer custom to different setups.
-    #     :return: InitLayer
-    #     """
-    #     return nn.Dense(features=self.dim_feature)
 
     def define_fixed_point_layer(self, mfvi):
         """
 
         :return:
         """
-        if not self.params.IFT:
-            return SigmoidFixedPointLayer(mfvi)
         if self.params.bwd_solver == 'normal_cg':
-            implicit_solver = partial(solve_normal_cg, tol=1e-3, maxiter=20)
+            implicit_solver = partial(solve_normal_cg, tol=self.params.bwd_tol, maxiter=self.params.bwd_maxiter)
         if self.params.fwd_solver == 'fpi':
             fixed_point_solver = partial(FixedPointIteration,
-                                         maxiter=4,
-                                         tol=1e-3, implicit_diff=True,
+                                         maxiter=self.params.fwd_maxiter,
+                                         tol=self.params.fwd_tol, implicit_diff=self.params.IFT,
                                          implicit_diff_solve=implicit_solver,
-                                         verbose=True)
+                                         verbose=self.params.is_verbose)
         return SigmoidImplicitLayer(mfvi=mfvi, fixed_point_solver=fixed_point_solver)
-
-    # def cross_entropy(self, q, S, neg_S):  # Eq. (5) in the paper
-    #     loss = - jnp.sum((S * jnp.log(q + 1e-12) + (1 - S) * jnp.log(1 - q + 1e-12)) * neg_S, axis=-1)
-    #     return loss.mean()
 
     def __call__(self, V, S, neg_S, **kwargs):
         """"returns cross-entropy loss."""
         bs, vs = V.shape[:2]
+        if self.params.data_name == 'celeba':
+            bs = int(bs / 8)
+            vs = self.params.v_size
         q = .5 * jnp.ones((bs, vs))  # ψ_0 <-- 0.5 * vector(1)
-        if not self.params.IFT:
-            q, _, _ = self.fixed_point_layer(q_init, V)
-        else:
-            q = self.fixed_point_layer(q, V)
 
-        # q = q_init
-        # iterations = 0
-        # last_err = float('inf')
-        #
-        # # iterate until convergence
-        # while iterations < 20:
-        #     q_next = self.mfvi(q, V)
-        #     err = jnp.linalg.norm(q - q_next)
-        #     print(err)
-        #     q = q_next
-        #     iterations += 1
-        #     # if err < 1e-3:
-        #     #     break
-        #     last_err = err
+        q = self.fixed_point_layer(q, V)
+        # print(f"q_out = {q}")
+        # assert jnp.all(q >= 0) and np.all(q <= 1), "Some elements of psi are not in [0, 1]!"
 
         loss = self.cross_entropy(q, S, neg_S)
         return loss

ImplicitEquiVSetFlax/model/trainer_module.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 # !pip install --quiet --upgrade flax optax
 import jax
 import jax.numpy as jnp
+import flax
 from jax import random
 from flax import linen as nn
 from flax.training import train_state, checkpoints
@@ -28,6 +29,9 @@ import torch.utils.data as data
 # If you run this code on Colab, remember to install pytorch_lightning
 # !pip install --quiet --upgrade pytorch_lightning
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+
+flax.config.update('flax_use_orbax_checkpointing', False)
+
 
 def append_dict(master_dict, sub_dict):
     """
@@ -45,10 +49,10 @@ def append_dict(master_dict, sub_dict):
     return master_dict
 
 
-def plot_dual_metric_dicts(train_loss_list, train_metric_dict, val_metric_dict, dataset):
+def plot_dual_metric_dicts(train_loss_list, train_metric_dict, val_metric_dict, dataset, amazon_cat=None):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
     ax2.plot(train_loss_list, '-', label=f"Training loss")
-    for line_style, ax, metric_dict in zip(['-','--'], [ax1, ax2], [train_metric_dict, val_metric_dict]):
+    for line_style, ax, metric_dict in zip(['-', '--'], [ax1, ax2], [train_metric_dict, val_metric_dict]):
         keys = list(metric_dict.keys())
         if len(metric_dict) == 2:
             metric1, metric2 = metric_dict[keys[0]], metric_dict[keys[1]]
@@ -71,7 +75,10 @@ def plot_dual_metric_dicts(train_loss_list, train_metric_dict, val_metric_dict, 
         ax2.legend(loc='upper right')
     plt.title(f'{dataset}')
     plt.tight_layout()
-    plt.savefig(f"./plots/plot_{dataset}.png")
+    plot_name = f"./plots/plot_{dataset}"
+    if dataset == 'amazon':
+        plot_name += f"_{amazon_cat}"
+    plt.savefig(plot_name + ".png")
 
 
 class TrainState(train_state.TrainState):
@@ -243,7 +250,7 @@ class TrainerModule(nn.Module):  # why did they define it without nn.Module?
         hparams = copy(self.optimizer_hparams)
 
         # Initialize optimizer
-        optimizer_name = hparams.pop('optimizer', 'adamw')
+        optimizer_name = hparams.pop('optimizer', 'adam')
         if optimizer_name.lower() == 'adam':
             opt_class = optax.adam
         elif optimizer_name.lower() == 'adamw':
@@ -264,12 +271,13 @@ class TrainerModule(nn.Module):  # why did they define it without nn.Module?
             end_value=0.01 * lr
         )
         # Clip gradients at max value, and evt. apply weight decay
-        transf = [optax.clip_by_global_norm(hparams.pop('gradient_clip', 10))]
-        if opt_class == optax.sgd and 'weight_decay' in hparams:  # wd is integrated in adamw
-            transf.append(optax.add_decayed_weights(hparams.pop('weight_decay', 0.0)))
+        transf = [optax.clip_by_global_norm(hparams.pop('clip', 10))]
+        if (opt_class == optax.sgd or opt_class == optax.adamw) and 'weight_decay' in hparams:  # wd is integrated in adamw
+            transf.append(optax.add_decayed_weights(hparams.pop('weight_decay', 1e-5)))
         optimizer = optax.chain(
             *transf,
-            opt_class(lr_schedule, **hparams)
+            # opt_class(lr_schedule, **hparams)
+            opt_class(learning_rate=lr, **hparams)
         )
         # Initialize training state
         self.state = TrainState.create(apply_fn=self.state.apply_fn,
@@ -349,8 +357,8 @@ class TrainerModule(nn.Module):  # why did they define it without nn.Module?
             append_dict(train_metric_dict, train_metrics)
             self.logger.log_metrics(train_metrics, step=epoch_idx)
             self.save_metrics('train', train_metrics)
-            print(f'Epoch {epoch_idx} | Training Loss: {train_loss["train/loss"]:.2f} '
-                  f'Training Jaccard Index: {train_metrics["train/jaccard"]:.2f}\n')
+            print(f'Epoch {epoch_idx} | Training Loss: {train_loss["train/loss"]:.2f} | '
+                  f'Training Jaccard Index: {train_metrics["train/jaccard"]:.2f}', end="")
 
             self.on_training_epoch_end(epoch_idx)
             # Validation every N epochs
@@ -361,25 +369,34 @@ class TrainerModule(nn.Module):  # why did they define it without nn.Module?
                 self.logger.log_metrics(val_metrics, step=epoch_idx)
                 self.save_metrics(f'eval_epoch_{str(epoch_idx).zfill(3)}', val_metrics)
 
-                print(f'Epoch {epoch_idx}|Val jc: {val_metrics["val/jaccard"]:.2f}\n')
+                print(f' | Val JC: {val_metrics["val/jaccard"]:.2f}', end="")
                 # Save best model
                 if self.is_new_model_better(val_metrics, best_eval_metrics):
                     best_eval_metrics = val_metrics
                     best_eval_metrics.update(train_loss)
                     best_eval_metrics.update(train_metrics)
-                    # self.save_model(step=epoch_idx)
-                    # self.save_metrics('best_eval', eval_metrics)
+                    self.save_model(step=epoch_idx)
+                    self.save_metrics('best_eval', val_metrics)
+                    bad_epochs = 0
+                    print(f'\t*Best model so far*')
+                else:
+                    bad_epochs += 1
+                    print(f'\tBad epoch %d' % bad_epochs)
+
+                if bad_epochs > self.model_hparams['params'].num_bad_epochs:
+                    break
             # Test best model if possible
         if test_loader is not None:
-            # self.load_model()
+            self.load_model()
             test_metrics = self.eval_model(test_loader, log_prefix='test/')
             self.logger.log_metrics(test_metrics, step=epoch_idx)
             self.save_metrics('test', test_metrics)
             best_eval_metrics.update(test_metrics)
-            print(f'Epoch {epoch_idx}|Test jc: {test_metrics["test/jaccard"]:.2f}\n')
+            print(f'\nTest JC of the best model: {test_metrics["test/jaccard"]:.2f}')
             # Close logger
         self.logger.finalize('success')
-        plot_dual_metric_dicts(train_loss_list, train_metric_dict, val_metric_dict, self.model_hparams['params'].data_name)
+        plot_dual_metric_dicts(train_loss_list, train_metric_dict, val_metric_dict,
+                               self.model_hparams['params'].data_name, self.model_hparams['params'].amazon_cat)
         return best_eval_metrics
 
     def train_epoch(self,

ImplicitEquiVSetFlax/utils/implicit.py
@@ -2,12 +2,9 @@ import jax
 import flax
 import flax.linen as nn
 import jax.numpy as jnp
-from functools import partial
-from jax import jit
 from jax import lax
 from jaxopt import AndersonAcceleration, FixedPointIteration
 from jaxopt.linear_solve import solve_gmres, solve_normal_cg
-# from model.set_functions_flax import SetFunction, MC_sampling
 from typing import Any, Callable
 
 
@@ -44,14 +41,12 @@ class SigmoidFixedPointLayer(nn.Module):
         # key = jax.random.PRNGKey(0)
         # params = self.mfvi.init(key, q_init, V)
 
-        # @partial(jit, static_argnames=['err'])
         def body(mdl, carry):
             q, iterations, last_err = carry
             q_next = mdl.mfvi.apply(mfvi_params, q, V)
             err = jnp.linalg.norm(q - q_next)
             return q_next, iterations + 1, err
 
-        # @partial(jit, static_argnames=['err'])
         def cond(mdl, carry):
             q, iterations, err = carry
             # break_condition = (err > 1e-3)
@@ -68,32 +63,15 @@ class SigmoidImplicitLayer(nn.Module):  # JAXOpt solves this for us using implic
     mfvi: Callable
     fixed_point_solver: Any  # AndersonAcceleration or FixedPointIteration
 
-    # def setup(self):
-    #     self.init = lambda rng, q, V: self.mfvi.init(rng, q, V)
-        # self.mfvi_params = self.param("mfvi_params", self.init, q_init, V)
-
-    # @partial(nn.transforms.vmap,
-    #          variable_axes={'params': None},
-    #          split_rngs={'params': False},
-    #          in_axes=(None, 0))
-    # @partial(
-    #     nn.transforms.scan,
-    #     variable_broadcast='params',
-    #     split_rngs={'params': False})
     @nn.compact
     def __call__(self, q_init, V, **kwargs):
-        # q = q_init
         init = lambda rng, q, V: self.mfvi.init(rng, q, V)
-        # # if self.is_initializing():
         mfvi_params = self.param("mfvi_params", init, q_init, V)
-        # # mfvi_params = self.mfvi.init(jax.random.key(42), q_init, V)
+        # print(f"q_init = {q_init}")
 
         def mfvi_apply(q, V, mfvi_params):
-            q, mfvi_params = self.mfvi.apply(mfvi_params, q, V, mutable=True)
-            return q
-        # def mfvi_apply(q, V):
-        #     q = self.mfvi(q, V)
-        #     return q
+            # q, mfvi_params = self.mfvi.apply(mfvi_params, q, V, mutable=True)
+            return self.mfvi.apply(mfvi_params, q, V)
 
         solver = self.fixed_point_solver(fixed_point_fun=mfvi_apply)
 
@@ -101,46 +79,4 @@ class SigmoidImplicitLayer(nn.Module):  # JAXOpt solves this for us using implic
             # print(solver.run(V_dummy, q, init_params).params)
             return solver.run(q, V, mfvi_params)[0]
 
-        # def batch_run(q, V):
-        #     # print(solver.run(V_dummy, q, init_params).params)
-        #     return solver.run(q, V)[0]
-
-        # We use vmap since we want to compute the fixed point separately for each
-        # example in the batch.
-        # return jax.vmap(batch_run, in_axes=(0, 0, None), out_axes=0)(q_init, V, mfvi_params)  # understand what vmap does and modify this line
         return batch_run(q_init, V, mfvi_params)
-        # return batch_run(q_init, V)
-
-
-# class SigmoidImplicitLayer(nn.Module):  # JAXOpt solves this for us using implicit differentiation
-#     mfvi: Callable
-#     fixed_point_solver: Any  # AndersonAcceleration or FixedPointIteration
-#
-#     # def setup(self):
-#     #     self.init = lambda rng, q, V: self.mfvi.init(rng, q, V)
-#         # self.mfvi_params = self.param("mfvi_params", self.init, q_init, V)
-#
-#     # @partial(nn.transforms.vmap,
-#     #          variable_axes={'params': None},
-#     #          split_rngs={'params': False},
-#     #          in_axes=(None, 0))
-#     @nn.compact
-#     def __call__(self, q_init, V, **kwargs):
-#         # q = q_init
-#         # init = lambda rng, q, V: self.mfvi.init(rng, q, V)
-#         # if self.is_initializing():
-#         # mfvi_params = self.param("mfvi_params", self.init, q_init, V)
-#
-#         def mfvi_apply(q, V):
-#             return self.mfvi(q, V)
-#
-#         solver = self.fixed_point_solver(fixed_point_fun=mfvi_apply)
-#
-#         def batch_run(q, V):
-#             # print(solver.run(V_dummy, q, init_params).params)
-#             return solver.run(q, V).params
-#
-#         # We use vmap since we want to compute the fixed point separately for each
-#         # example in the batch.
-#         return jax.vmap(batch_run, in_axes=(None, 1), out_axes=0)(q_init, V)  # understand what vmap does and modify this line
-#         # return batch_run(q_init, V)